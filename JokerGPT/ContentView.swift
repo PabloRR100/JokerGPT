5cfdc66ee58c2b95c34059d5316bf7ba83257f98
p_ruizruiz <p_ruizruiz@apple.com>
2023-08-25 20:14:16 +0200

    Initial Commit

---
+++ JokerGPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokerGPT
+//
+//  Created by Pablo Ruiz on 25/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JokerGPT/JokerGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokerGPTApp.swift
+//  JokerGPT
+//
+//  Created by Pablo Ruiz on 25/8/23.
+//
+
+import SwiftUI
+
+@main
+struct JokerGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
